41551cdefe6229723619eb10024754e89a8aa5a5
pramod <ph.796@yahoo.com>
2025-03-01 10:13:41 +0530

    modified:   config/config.json 	modified:   kpimon.go

--- kpimon.go
+++ kpimon.go
@@ -7,9 +7,9 @@ import "time"
 func main() {
 	time.Sleep(15* time.Second)
 	fmt.Printf("In Main ------")
-	c := control.NewControl()
+	_ = control.NewControl()
 	fmt.Printf("c.Run()")
-	c.Run()
+	// c.Run()
 	/*
 	err:=control.Test()
 	if err != nil {
